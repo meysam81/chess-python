b99fb8b245181e678f120f0550b14633b90c56fd
Meysam <MeysamAzad81@yahoo.com>
2019-02-17 10:53:42 +0330

    remove abbr2piece, as we don't need it anymore

--- piece.py
+++ piece.py
@@ -2,27 +2,6 @@ from copy import deepcopy
 from error import ColorError, InvalidMove, InvalidPiece
 
 
-def abbr2piece(abbreviation):
-    abbr = abbreviation.lower()
-    color = "w" if abbreviation.isupper() else "b"
-    if abbr == "-":
-        return None
-    elif abbr == "p":
-        return Pawn(color)
-    elif abbr == "n":
-        return Knight(color)
-    elif abbr == "b":
-        return Bishop(color)
-    elif abbr == "r":
-        return Rook(color)
-    elif abbr == "q":
-        return Queen(color)
-    elif abbr == "k":
-        return King(color)
-    else:
-        raise InvalidPiece(f"Unknown piece: {abbreviation}")
-
-
 class Piece(object):
     """ base class of chess pieces """
 
@@ -292,10 +271,3 @@ class King(Piece):
         self.position = dest
 
 
-def main():
-    piece = abbr2piece("P")
-    print(piece == "P")
-
-
-if __name__ == '__main__':
-    main()
